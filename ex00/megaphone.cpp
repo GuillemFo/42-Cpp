a953a8282d2ae07712c975b242b912ccf0917f75
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-04-30 14:35:02 +0200

    ok

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,23 +6,26 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 08:52:54 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/04/30 13:15:22 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/04/30 14:34:17 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-
-
 int main(int ac, char **av)
 {
-	if (ac != 2)
+	if (ac == 1)
 	{
-		std::cout << "Wrong number of arguments\n";
-		return (-1);
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (1);
 	}
-	std::cout << av[1];
+	for (int j = 1; av[j] != NULL; j++)
+	{
+		for (int i = 0; av[j][i] != '\0'; i++)
+		{
+			std::cout << (char)std::toupper(av[j][i]);
+		}
+	}
 	std::cout << "\n";
-
 	return (0);
 }
