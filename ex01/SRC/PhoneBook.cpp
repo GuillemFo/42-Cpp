adb08c6233200f213f9ce4edfca8795546321352
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-04-30 15:14:05 +0200

    new

--- ex01/SRC/PhoneBook.cpp
+++ ex01/SRC/PhoneBook.cpp
@@ -1,32 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/30 08:52:54 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/04/30 15:00:44 by gforns-s         ###   ########.fr       */
+/*   Created: 2024/04/30 14:51:32 by gforns-s          #+#    #+#             */
+/*   Updated: 2024/04/30 14:51:58 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 
-int main(int ac, char **av)
+
+int	main()
 {
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		return (1);
-	}
-	for (int j = 1; av[j] != NULL; j++)
-	{
-		for (int i = 0; av[j][i] != '\0'; i++)
-		{
-			std::cout << (char)std::toupper(av[j][i]);
-		}
-	}
-	std::cout << std::endl;
-	
-	return (0);
+
+
 }
