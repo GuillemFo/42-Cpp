a901d94c8c0f73ff817923d9719f7d15546017d1
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-04-30 13:16:26 +0200

    testing prints

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,16 +6,23 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 08:52:54 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/04/30 09:59:12 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/04/30 13:15:22 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 
-int	main()
+
+
+int main(int ac, char **av)
 {
-	int	a;
+	if (ac != 2)
+	{
+		std::cout << "Wrong number of arguments\n";
+		return (-1);
+	}
+	std::cout << av[1];
+	std::cout << "\n";
 
-	a = 20;
-
-	return (a);
+	return (0);
 }
