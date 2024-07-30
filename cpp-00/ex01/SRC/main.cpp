d71e5f0348c9e12c05ad5ed21474f65fce271507
Guillem Forns Sant <gforns-s@car10s4.42barcelona.com>
2024-07-30 09:57:05 +0200

    renammed folder and working on cpp

--- cpp-00/ex01/SRC/main.cpp
+++ cpp-00/ex01/SRC/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 13:52:25 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/07/29 14:39:48 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/07/30 09:30:01 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@
 int	main()
 {
 	Contacts test1(5, "bob", "test", "nick");
-	std::cout << test1.index << " | " << test1.name << " | "  << test1.surname << " | " << test1.nick << std::endl;
+	std::cout << test1.getIndex << " | " << test1.getName << " | "  << test1.getSurname << " | " << test1.getNick << std::endl;
 }
