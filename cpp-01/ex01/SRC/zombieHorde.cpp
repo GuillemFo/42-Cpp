70c85204c66b68e0e3521260d9181b7934c34fb0
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-08-29 13:26:13 +0200

    testing

--- cpp-01/ex01/SRC/zombieHorde.cpp
+++ cpp-01/ex01/SRC/zombieHorde.cpp
@@ -6,8 +6,17 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 13:01:34 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/08/29 13:06:42 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/08/29 13:25:43 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie* zombieHorde( int N, std::string name )
+{
+	Zombie* s = new Zombie[N];
+	while (s[i])
+	{
+		
+	}
+}
