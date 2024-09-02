90639cc33ba3e620f0ff62593bb0aa1feaaada03
Guillem Forns Sant <gforns-s@car11s5.42barcelona.com>
2024-09-02 15:09:00 +0200

    preparing files

--- cpp-01/ex03/Include/Weapon.hpp
+++ cpp-01/ex03/Include/Weapon.hpp
@@ -6,8 +6,21 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 14:46:54 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/02 14:48:39 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/02 15:00:38 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <string>
+
+class Weapon
+{
+	private:
+		std::string type;
+	public:
+		Weapon();
+		~Weapon();
+		const std::string& getType();
+		void setType(std::string set);
+	
+}
--- cpp-01/ex03/SRC/Weapon.cpp
+++ cpp-01/ex03/SRC/Weapon.cpp
@@ -6,8 +6,26 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 14:47:11 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/02 14:49:58 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/02 15:07:01 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
+
+Weapon::Weapon()
+{
+	type = "";
+}
+Weapon::~Weapon()
+{
+	
+}
+const std::string& Weapon::getType()
+{
+	return (type);	
+}
+
+void Weapon::setType(std::string set)
+{
+	type = set;
+}
