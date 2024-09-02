7e8ed2cdb8074f5759ccecb98d2cefdb691028f1
Guillem Forns Sant <gforns-s@car11s5.42barcelona.com>
2024-09-02 13:54:46 +0200

    ok??

---
+++ cpp-01/ex02/SRC/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 13:25:03 by gforns-s          #+#    #+#             */
+/*   Updated: 2024/09/02 13:54:15 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int	main(void)
+{
+	std::string s = "HI THIS IS BRAIN";
+	std::string *stringPTR = &s;
+	std::string &stringREF = s;
+
+	std::cout << "Memory address of the string variable: " << &s << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "The value of the string variable: " << s << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+	
+	
+	return (0);
+}
