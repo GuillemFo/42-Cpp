f7e88bccdb084f040b10e517402c3589afae7310
Guillem Forns Sant <gforns-s@car11s5.42barcelona.com>
2024-09-02 14:53:24 +0200

    ok

--- cpp-01/ex03/SRC/main.cpp
+++ cpp-01/ex03/SRC/main.cpp
@@ -6,7 +6,9 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 14:41:10 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/02 14:41:12 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/02 14:51:09 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanA.hpp"
+#include "HumanB.hpp"
