5e857d74f0ec4272f81553849408aca349c781ec
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-09-05 10:13:18 +0200

    starting last project

---
+++ cpp-01/ex05/Include/Harl.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 09:55:23 by gforns-s          #+#    #+#             */
+/*   Updated: 2024/09/05 10:04:22 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <iostream>
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
+};
---
+++ cpp-01/ex05/SRC/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 09:55:29 by gforns-s          #+#    #+#             */
+/*   Updated: 2024/09/05 10:02:04 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main()
+{
+
+	return (0);
+}
+
+
+/*
+The goal of this exercise is to use pointers to member functions. This is not a
+suggestion
+*/
