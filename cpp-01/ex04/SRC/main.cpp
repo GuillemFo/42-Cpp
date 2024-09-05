753c57e18d90841cad5ca3fa317c50cae754e9be
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-09-05 09:53:37 +0200

    ok

--- cpp-01/ex04/SRC/main.cpp
+++ cpp-01/ex04/SRC/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 08:22:51 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/04 12:12:57 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/05 09:52:36 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,48 @@
 
 int	main(int ac, char **av)
 {
-	if (ac < 4)
+	if (ac < 4 || ac > 4)
 	{
 		std::cout << "Wrong number of arguments" << std::endl;
 		return (1);
 	}
-		std::string filename = av[1];
-		std::string s1 = av[2];
-		std::string s2 = av[3];
-		if (filename.empty() || s1.empty() || s2.empty())
+	std::string filename = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	if (filename.empty() || s1.empty() || s2.empty())
+	{
+		std::cout << "Program cannot tolerate empty fields" << std::endl;
+		return (1);
+	}
+	std::string	tmp_name = filename + ".replace";
+	std::ifstream in_file(filename.c_str());
+	std::ofstream out_file(tmp_name.c_str());
+	if (in_file.is_open() && out_file.is_open())
+	{
+		std::string	line;
+		while (std::getline(in_file, line))
 		{
-			std::cout << "Program cannot tolerate empty fields" << std::endl;
-			return (1);
-		}
-		std::ifstream in_file(filename); // c_str ??
-		std::ofstream out_file(filename + ".replace");
-		if (in_file.is_open() && out_file.is_open())
-		{
-			std::string line;
-			std::string new_line;
-			std::string tmp;
-			while (std::getline(in_file, line))
+			size_t	start = line.find(s1);
+			if (start != std::string::npos)
 			{
-				size_t	start = line.find(s1);
-				size_t len = s1.length();
+				size_t	len_s1 = s1.length();
+				size_t	len_newl = 0;
+				std::string	new_line = line.substr(len_newl, start);
 				while (start != std::string::npos)
 				{
+					out_file << new_line + s2;
+					line = line.substr(start + len_s1);
+					start = line.find(s1);
 					new_line = line.substr(0, start);
-					tmp = line.substr(start + len);
-					new_line = new_line + tmp;
-					std::cout << new_line << std::endl;
 				}
+				out_file << line.substr(0);
 			}
+			else
+				out_file << line;
+			out_file << std::endl;	
 		}
-		
-		
+		in_file.close();
+		out_file.close();
+	}
 	return (0);
 }
