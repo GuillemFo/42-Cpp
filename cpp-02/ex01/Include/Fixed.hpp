a9243464bae1dbf032cd8850fbf5821004d001fc
Guillem Forns Sant <gforns-s@cbr8s3.42barcelona.com>
2024-09-13 11:59:59 +0200

    starting ex01

--- cpp-02/ex01/Include/Fixed.hpp
+++ cpp-02/ex01/Include/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 08:49:44 by codespace         #+#    #+#             */
-/*   Updated: 2024/09/13 11:02:15 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/13 11:09:07 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,12 @@ class Fixed
 		Fixed();
 		~Fixed();
 		Fixed(const Fixed& a);
-		Fixed& 	operator=(const Fixed &b);
+		Fixed(const int a);
+		Fixed(const float a);
+		Fixed	& operator=(const Fixed &b);
+		Fixed	&operator<<(const Fixed &b);
 		int		getRawBits( void ) const;
 		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
 };
---
+++ cpp-02/ex01/SRC/Fixed.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 09:29:27 by codespace         #+#    #+#             */
+/*   Updated: 2024/09/13 11:58:52 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called for Fixed" << std::endl;
+	fixed = 0;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor for Fixed was called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& a)
+{
+	std::cout << "Copy constructor called for Fixed" << std::endl;
+	*this = a;
+}
+
+Fixed& Fixed::operator=(const Fixed& b)
+{
+	std::cout << "Copy assignment operator called for Fixed" << std::endl;
+	if (this != &b)
+		this->setRawBits(b.getRawBits());
+	return (*this);
+}
+
+int	Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (fixed);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	fixed = raw;
+}
+
+float	Fixed::toFloat(void)const
+{
+
+}
+
+int		Fixed::toInt(void)const
+{
+	
+}
