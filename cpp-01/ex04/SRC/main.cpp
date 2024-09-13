2ca729d1732c4b8b984db0dc58c8b5408608413d
Guillem Forns Sant <gforns-s@cbr8s3.42barcelona.com>
2024-09-13 10:13:50 +0200

    fixed makefiles

--- cpp-01/ex04/SRC/main.cpp
+++ cpp-01/ex04/SRC/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 08:22:51 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/12 10:40:20 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/13 10:01:02 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,35 +29,40 @@ int	main(int ac, char **av)
 		std::cout << "Program cannot tolerate empty fields" << std::endl;
 		return (1);
 	}
-	std::string	tmp_name = filename + ".replace";
 	std::ifstream in_file(filename.c_str());
-	std::ofstream out_file(tmp_name.c_str());
-	if (in_file.is_open() && out_file.is_open())
+	if (in_file.is_open())
 	{
-		std::string	line;
-		while (std::getline(in_file, line))
+		std::string	tmp_name = filename + ".replace";
+		std::ofstream out_file(tmp_name.c_str());
+		if (in_file.is_open() && out_file.is_open())
 		{
-			size_t	start = line.find(s1);
-			if (start != std::string::npos)
+			std::string	line;
+			while (std::getline(in_file, line))
 			{
-				size_t	len_s1 = s1.length();
-				size_t	len_newl = 0;
-				std::string	new_line = line.substr(len_newl, start);
-				while (start != std::string::npos)
+				size_t	start = line.find(s1);
+				if (start != std::string::npos)
 				{
-					out_file << new_line + s2;
-					line = line.substr(start + len_s1);
-					start = line.find(s1);
-					new_line = line.substr(0, start);
+					size_t	len_s1 = s1.length();
+					size_t	len_newl = 0;
+					std::string	new_line = line.substr(len_newl, start);
+					while (start != std::string::npos)
+					{
+						out_file << new_line + s2;
+						line = line.substr(start + len_s1);
+						start = line.find(s1);
+						new_line = line.substr(0, start);
+					}
+					out_file << line.substr(0);
 				}
-				out_file << line.substr(0);
+				else
+					out_file << line;
+				out_file << std::endl;	
 			}
-			else
-				out_file << line;
-			out_file << std::endl;	
+			in_file.close();
+			out_file.close();
 		}
-		in_file.close();
-		out_file.close();
 	}
+	else
+		std::cout << "Error" << std::endl;
 	return (0);
 }
