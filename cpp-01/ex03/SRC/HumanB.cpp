806a2dfea5ae9ff635ef2fd234ffb9d402f17a30
Guillem Forns Sant <gforns-s@cbr1s5.42barcelona.com>
2024-09-17 09:51:40 +0200

    chnged atack without weapon

--- cpp-01/ex03/SRC/HumanB.cpp
+++ cpp-01/ex03/SRC/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 14:47:07 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/13 10:52:09 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/17 09:45:20 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void HumanB::setWeapon(Weapon& wnm)
 void HumanB::attack()const
 {
 	if (HumanB::w_typeB == NULL)
-		std::cout << name << " attacks with their " << "hands" << std::endl;
+		std::cout << name << " attacks with their hands" << std::endl;
 	else
 		std::cout << name << " attacks with their " << w_typeB->getType() << std::endl;
 }
