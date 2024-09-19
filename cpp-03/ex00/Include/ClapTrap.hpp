27d89882c05b29650a2c0c2010636d01513b23f0
Guillem Forns Sant <gforns-s@cbr6s3.42barcelona.com>
2024-09-19 11:35:54 +0200

    preparing ex00

--- cpp-03/ex00/Include/ClapTrap.hpp
+++ cpp-03/ex00/Include/ClapTrap.hpp
@@ -6,7 +6,22 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 11:28:12 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/18 11:28:13 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/19 11:35:33 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <string>
+
+class ClapTrap
+{
+	private:
+		std::string		_name;
+		unsigned int	_hit_pts;
+		unsigned int	_energy;
+		unsigned int	_attack;
+	public:
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int ammount);
+		void	beRepaired(unsigned int ammount);
+};
