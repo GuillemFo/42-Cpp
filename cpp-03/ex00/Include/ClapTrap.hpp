eae00b9aedb6b86062a4f153ffcb52a24c1eac2d
Guillem Forns Sant <gforns-s@cbr8s3.42barcelona.com>
2024-09-23 13:26:13 +0200

    work in progress

--- cpp-03/ex00/Include/ClapTrap.hpp
+++ cpp-03/ex00/Include/ClapTrap.hpp
@@ -6,12 +6,13 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 11:28:12 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/19 11:35:33 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/23 10:32:06 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <string>
+#include <iostream>
 
 class ClapTrap
 {
@@ -21,7 +22,12 @@ class ClapTrap
 		unsigned int	_energy;
 		unsigned int	_attack;
 	public:
-		void	attack(const std::string& target);
-		void	takeDamage(unsigned int ammount);
-		void	beRepaired(unsigned int ammount);
+		ClapTrap(std::string a);
+		~ClapTrap();
+		ClapTrap(const ClapTrap &a);
+		
+		ClapTrap	&operator=(const ClapTrap &a);
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int ammount);
+		void		beRepaired(unsigned int ammount);
 };
--- cpp-03/ex00/SRC/ClapTrap.cpp
+++ cpp-03/ex00/SRC/ClapTrap.cpp
@@ -6,8 +6,74 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 11:28:02 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/09/19 08:14:02 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/09/23 13:25:56 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string a) : _hit_pts(10), _energy(10), _attack(0)
+{
+	std::cout << "Default constructor called for " << a << std::endl;
+	_name = a;
+}
+
+ClapTrap::~ClapTrap()
+{
+
+}
+
+ClapTrap::ClapTrap(const ClapTrap &a)
+{
+	std::cout << "This is a copy constructor" << std::endl;
+	*this = a;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &a)
+{
+	if (this != &a)
+	{
+		this->_attack = a._attack;
+		this->_energy = a._energy;		//have to double check if i need setters and getters (check if setter and getter is per class or element)
+		this->_hit_pts = a._hit_pts;
+		this->_name = a._name;
+	}
+	return (*this);
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (_energy == 0)
+	{
+		std::cout << "Not enough energy to attack" << std::endl;
+		return;
+	}
+	_energy = _energy - 1;
+	std::cout << this->_name << "attacks" << &target << ", causing " << _attack << "points of damage!" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int ammount)
+{
+	if (_hit_pts == 0)
+	{
+		std::cout << "the target is already dead" << std::endl;
+		return;
+	}
+	if (_hit_pts < ammount)
+	{
+		_hit_pts = 0;
+		std::cout << "the target died" << std::endl;
+	}
+	_hit_pts = _hit_pts - ammount;
+}
+
+void	ClapTrap::beRepaired(unsigned int ammount)
+{
+	if (_energy == 0)
+	{
+		std::cout << "Not enough energy to repair" << std::endl;
+		return;
+	}
+	_energy = _energy - 1;
+	_hit_pts = _hit_pts + ammount;
+}
