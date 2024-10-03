d1ea003074e205f3e67a9863a0489da7ea9ad534
Guillem Forns Sant <gforns-s@cbr1s3.42barcelona.com>
2024-10-03 09:04:12 +0200

    ok

--- cpp-04/ex00/Include/Cat.hpp
+++ cpp-04/ex00/Include/Cat.hpp
@@ -6,11 +6,12 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 10:42:03 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/03 08:49:59 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/03 09:03:44 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <iostream>
 #include "Animal.hpp"
 
 class Cat : public Animal
--- cpp-04/ex00/Include/Dog.hpp
+++ cpp-04/ex00/Include/Dog.hpp
@@ -6,13 +6,13 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 10:41:18 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/03 08:49:54 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/03 09:03:51 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#include "Animal.hpp"
 #include <iostream>
+#include "Animal.hpp"
 
 class Dog : public Animal
 {
