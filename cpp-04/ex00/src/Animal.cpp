682453fa8b07bd50e339207b22badc30acc0a656
Guillem Forns Sant <gforns-s@cbr1s3.42barcelona.com>
2024-10-03 09:01:59 +0200

    ok

---
+++ cpp-04/ex00/src/Animal.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 10:34:46 by gforns-s          #+#    #+#             */
+/*   Updated: 2024/10/03 08:49:35 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal() : _type("default")
+{
+	std::cout << "Animal constructor default" << std::endl;
+}
+
+Animal::Animal(std::string a)
+{
+	this->_type = a;
+	std::cout << "Animal constructor " << _type << std::endl;
+}
+
+Animal::Animal(const Animal &other)
+{
+	*this = other;
+	std::cout << "Animal copy constructor" << std::endl;
+}
+
+Animal	&Animal::operator=(const Animal &other)
+{
+	if (this != &other)
+		this->_type = other._type;
+	return (*this);
+}
+
+std::string	Animal::getType() const
+{
+	return (this->_type);
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
+
+void Animal::makeSound() const
+{
+	std::cout << "Default sound" << std::endl;
+}
+
