a49a566f19a06683b2a8f540b7b39957c5277e9f
Guillem Forns Sant <gforns-s@cbr1s3.42barcelona.com>
2024-10-09 09:13:18 +0200

    work in progress

--- cpp-05/ex00/Include/Bureaucrat.hpp
+++ cpp-05/ex00/Include/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 01:18:21 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/05 01:20:23 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/09 09:12:38 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,14 @@
 class Bureaucrat
 {
 	protected:
-
+		const std::string name;
+		int grade;
 	public:
+		Bureaucrat();
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat &operator=(const Bureaucrat &other);
 
+		const std::string getName() const;
+		int getGrade();
 };
