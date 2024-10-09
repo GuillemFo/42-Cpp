e6af0033d6571173fe2ddca92aa57eda01e0bfc3
Guillem Forns Sant <gforns-s@cbr1s3.42barcelona.com>
2024-10-09 10:00:48 +0200

    work in progress

--- cpp-05/ex00/Include/Bureaucrat.hpp
+++ cpp-05/ex00/Include/Bureaucrat.hpp
@@ -6,12 +6,13 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 01:18:21 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/09 09:12:38 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/09 09:46:38 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+#include <string>
 
 class Bureaucrat
 {
@@ -19,11 +20,12 @@ class Bureaucrat
 		const std::string name;
 		int grade;
 	public:
-		Bureaucrat();
+		Bureaucrat(const std::string nm, int i);
 		~Bureaucrat();
 		Bureaucrat(const Bureaucrat &other);
 		Bureaucrat &operator=(const Bureaucrat &other);
 
 		const std::string getName() const;
 		int getGrade();
+		int setGrade();
 };
--- cpp-05/ex00/src/Bureaucrat.cpp
+++ cpp-05/ex00/src/Bureaucrat.cpp
@@ -6,7 +6,46 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 01:18:18 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/05 01:18:19 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/09 10:00:25 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string nm, int i) : name(nm), grade(i)
+{
+	std::cout << "Bureaucrat " << this->name << " constructor" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat " << this->name << " destructor" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other)
+{
+	*this = other;
+	std::cout << "Copy constructor " << &other << std::endl;
+
+}
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+	if (this != &other)
+	{
+		std::cout << "help me :(" << std::endl;
+		//setname and set grade but name is const so cannot change it??
+	}
+	return (*this);
+}
+
+const std::string Bureaucrat::getName() const
+{
+	return (this->name);
+}
+
+int Bureaucrat::getGrade()
+{
+	return (this->grade);	
+}
+
 
