9e9e5b5732832a70959f4e8f56aee16f5e906fb5
Guillem Forns Sant <gforns-s@car8s6.42barcelona.com>
2024-10-23 09:02:37 +0200

    work in progress

--- cpp-05/ex00/Include/Bureaucrat.hpp
+++ cpp-05/ex00/Include/Bureaucrat.hpp
@@ -6,19 +6,20 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 01:18:21 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/09 11:09:17 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/23 08:47:31 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 class Bureaucrat
 {
 	protected:
-		const std::string name;
-		int grade;
+		const std::string _name;
+		int _grade;
 	public:
 		Bureaucrat(const std::string nm, int i);
 		~Bureaucrat();
@@ -29,5 +30,14 @@ class Bureaucrat
 		int getGrade();
 		int incrementGrade();
 		int decrementGrade();
-		
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const throw();		
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const throw();		
+		};
 };
--- cpp-05/ex00/src/Bureaucrat.cpp
+++ cpp-05/ex00/src/Bureaucrat.cpp
@@ -6,25 +6,24 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 01:18:18 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/09 11:10:30 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/23 08:53:33 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(const std::string nm, int i) : name(nm)
+Bureaucrat::Bureaucrat(const std::string nm, int i) : _name(nm)
 {
-	std::cout << "Bureaucrat " << this->name << " constructor" << std::endl;
+	std::cout << "Bureaucrat " << this->_name << " constructor" << std::endl;
 	try
 	{
 		if (i > 150)
-			throw Bureaucrat::GradeTooHighException;
+			throw Bureaucrat::GradeTooHighException("Grade too high");
 		else if (i < 1)
-			throw Bureaucrat::GradeTooLowException;
+			throw Bureaucrat::GradeTooLowException("Grade too low");
 		else if (i >= 1 && i <= 150)
-			grade = i;
-		else
-			throw Bureaucrat::SendHelp;
+			_grade = i;
+
 	}
 	catch(int nbr)
 	{
@@ -36,7 +35,7 @@ Bureaucrat::Bureaucrat(const std::string nm, int i) : name(nm)
 
 Bureaucrat::~Bureaucrat()
 {
-	std::cout << "Bureaucrat " << this->name << " destructor" << std::endl;
+	std::cout << "Bureaucrat " << this->_name << " destructor" << std::endl;
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &other)
@@ -50,18 +49,27 @@ Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
 	std::cout << "help me :(" << std::endl;
 	if (this != &other)
 	{
-		this->grade = other.grade;
+		this->_grade = other._grade;
 	}
 	return (*this);
 }
 
 const std::string Bureaucrat::getName() const
 {
-	return (this->name);
+	return (this->_name);
 }
 
 int Bureaucrat::getGrade()
 {
-	return (this->grade);	
+	return (this->_grade);	
 }
 
+const char *Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+	return ("Grade too high, range [1 - 150].");
+}
+
+const char *Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+	return ("Grade too low, range [1 - 150].");
+}
