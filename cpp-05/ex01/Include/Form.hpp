bbd6e91ab214483be9a080d8aea32f1452fc1eb6
Guillem Forns Sant <gforns-s@car8s6.42barcelona.com>
2024-10-23 11:32:15 +0200

    this is hard

--- cpp-05/ex01/Include/Form.hpp
+++ cpp-05/ex01/Include/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:08:18 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/23 11:16:12 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/23 11:30:54 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,32 @@
 class Form
 {
 	private:
-		const std::string _name;
-		bool	sign;
-		const int grad_rec;
-		const int grad_exec;
+		const std::string	_name;
+		const int			_reqGrade;
+		const int 			_execGrade;
+		bool				_sign;
 	public:
+		Form();
+		Form(std::string nm, int req, int exec, bool def_status);
+		~Form();
+		Form(const Form &other);
+		Form &operator=(const Form &a);
+		
+		std::string getName() const;
+		int	getReqGrade() const;
+		int	getExecGrade() const;
+		bool getSign();
+		void beSigned(Bureaucrat &a); ///pending to investigate
 
-	class GradeTooHighException
+	class GradeTooHighException : public std::exception
 	{
-		
+		public:
+			const char *what() const throw();
 	};
-	class GradeTooLowException
+	class GradeTooLowException : public std::exception
 	{
-		
+		public:
+			const char *what() const throw();
 	};
 };
+std::ostream &operator<<(std::ostream &val, const Form &who);
