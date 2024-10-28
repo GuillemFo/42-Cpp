64cf0eb5a73540e7c031649d6521ac1bb0b2ae70
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-10-28 13:53:43 +0100

    saving

--- cpp-06/ex00/Include/ScalarConverter.hpp
+++ cpp-06/ex00/Include/ScalarConverter.hpp
@@ -6,14 +6,20 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:51:04 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/28 12:54:58 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/28 13:48:51 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <iostream>
 
 class ScalarConverter
 {
+	private:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &other);
+		~ScalarConverter();
+		ScalarConverter &operator=(const ScalarConverter &cpy);
 	public:
-		
+		static void convert(const std::string in);
 };
--- cpp-06/ex00/src/ScalarConverter.cpp
+++ cpp-06/ex00/src/ScalarConverter.cpp
@@ -6,9 +6,35 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:51:01 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/10/28 12:55:53 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/10/28 13:50:57 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+ScalarConverter::ScalarConverter()
+{
+
+}
+
+ScalarConverter::~ScalarConverter()
+{
+
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other)
+{
+	*this = other
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
+{
+	(void)other;
+	return (*this);
+}
+
+void	ScalarConverter::convert(const std::string in)
+{
+
+
+}
