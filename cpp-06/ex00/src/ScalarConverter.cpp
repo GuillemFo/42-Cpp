b9c55710328f587c85a9b3e2183b1044c21eb3a1
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-11-18 11:07:25 +0100

    int bounds issues

--- cpp-06/ex00/src/ScalarConverter.cpp
+++ cpp-06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:51:01 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/17 00:55:05 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/18 11:06:52 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,17 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
 	return (*this);
 }
 
-
 bool	ckCha(const std::string &str)
 {
 	if (str.length() == 1 && isprint(str[0]))
 		return (true);
 	return (false);
 }
-bool	ckInt(const std::string &str)
+
+bool	ckInt(const std::string &str) // Error on max int min int and negatives!!!
 {
 	std::stringstream tmp(str);
-	int result;
+	long result;
 
 	tmp >> result;
 	if (tmp.fail() || !tmp.eof())
@@ -51,6 +51,8 @@ bool	ckInt(const std::string &str)
 		tmp.clear();
 		return (false);
 	}
+	if (result < std::numeric_limits<int>::min() || result > std::numeric_limits<int>::max())
+        return false;
 	return (true);
 }
 
@@ -122,18 +124,75 @@ char    getType(const std::string &str)
     return ('n');
 }
 
-void ScalarConverter::convert(const std::string& s)
+void ScalarConverter::convert(const std::string& str)
 {
-    char type = getType(s);
+	char type = getType(str);
 
 	if (type == 'c')
-		std::cout << "is char" << std::endl;
+	{
+		if (isprint(str[0]))
+			std::cout << "char: " << static_cast<char>(str[0]) << std::endl;
+		else
+			std::cout << "char: " << "Non displayable" << std::endl;
+		std::cout << "int: " << static_cast<int>(str[0]) << std::endl;
+		std::cout << "float: " << static_cast<float>(str[0]) << std::endl;
+		std::cout << "double: " << static_cast<double>(str[0]) << std::endl;
+	}
+	
+
 	else if (type == 'i')
-		std::cout << "is int" << std::endl;
+	{
+		int i;
+		i = atoi(str.c_str());
+		if (isprint(i))
+			std::cout << "char: " << static_cast<char>(i) << std::endl;
+		else
+			std::cout << "char: " << "Non displayable" << std::endl;
+		std::cout << "int: " << static_cast<int>(i) << std::endl;
+		std::cout << "float: " << static_cast<float>(i) << std::endl;
+		std::cout << "double: " << static_cast<double>(i) << std::endl;
+	}
+
+
 	else if (type == 'f')
-		std::cout << "is float" << std::endl;
+	{
+		std::string tmp = str;
+		tmp.resize(tmp.length() -1);
+		if (str == "-inff" || str == "+inff" || str == "nanf")
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: " << tmp << "f" << std::endl;
+			std::cout << "double: " << tmp << std::endl;
+            return ;
+		}
+		/*
+		if (isprint(static_cast<int>()))
+			std::cout << "char: " << static_cast<char>() << std::endl;
+		else
+			std::cout << "char: " << "Non displayable" << std::endl;
+		std::cout << "int: " << static_cast<int>() << std::endl;
+		std::cout << "float: " << static_cast<float>() << "f" << std::endl;
+		std::cout << "double: " << tmp << std::endl;
+		*/
+		std::cout << "Is Float" << std::endl;
+	}
+
+
 	else if (type == 'd')
+	{
+		if (str == "-inf" || str == "+inf" || str == "nan")
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: " << str << "f" << std::endl;
+			std::cout << "double: " << str << std::endl;
+			return ;
+		}
 		std::cout << "is double" << std::endl;
+	}
+
+	
 	else if (type == 'n')
 		std::cout << "is trash" << std::endl;
 }
