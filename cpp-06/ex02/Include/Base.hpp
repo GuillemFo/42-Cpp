0f8ed650476cf0594b0f5491f3e978284f5505ce
Guillem Forns Sant <gforns-s@car10s6.42barcelona.com>
2024-11-21 13:14:14 +0100

    missing reference identifier

--- cpp-06/ex02/Include/Base.hpp
+++ cpp-06/ex02/Include/Base.hpp
@@ -6,13 +6,28 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:25:53 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/18 15:26:09 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/21 13:02:19 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <exception>
+#include <ctime>
 
-class Base
+class	Base
 {
-
+	public:
+		virtual ~Base();
 };
+
+class	A : public Base{};
+class	B : public Base{};
+class	C : public Base{};
+
+Base 	*generate(void);
+void 	identify(Base* p);
+void 	identify(Base& p);
+
--- cpp-06/ex02/src/Base.cpp
+++ cpp-06/ex02/src/Base.cpp
@@ -6,9 +6,59 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:26:19 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/18 15:26:38 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/21 13:02:39 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
+Base::~Base()
+{
+	
+}
+
+Base *generate(void)
+{
+	srand(static_cast<unsigned int>(time(0)));
+	int num = rand() %3;
+
+	if (num == 0)
+		return (new A);
+
+	else if (num == 1)
+		return (new B);
+
+	else if (num == 2)
+		return (new C);
+		
+	return (new A);
+}
+
+
+void identify(Base *p)
+{
+	try
+	{
+
+		A *_A = dynamic_cast<A *>(p);
+		B *_B = dynamic_cast<B *>(p);
+		C *_C = dynamic_cast<C *>(p);
+
+		if (_A)
+			std::cout << "A Class identified" << std::endl;
+		else if (_B)
+			std::cout << "B Class identified" << std::endl;
+		else if (_C)
+			std::cout << "C Class identified" << std::endl;
+	}
+	catch (...)
+	{
+	}
+	
+}
+
+void identify(Base& p)
+{
+	(void)p;
+	std::cout << "missing still\n";	
+}
