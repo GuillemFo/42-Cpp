79d5dcbea7425f5a7c86cbe914bacf24afb28316
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-11-25 11:47:55 +0100

    preparing main

--- cpp-07/ex02/Include/Array.hpp
+++ cpp-07/ex02/Include/Array.hpp
@@ -6,14 +6,62 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 09:43:25 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/25 09:44:53 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/25 11:47:19 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <stdexcept>
+#include <iostream>
+#include <string>
 
+template< typename T >
 class Array
 {
+	private:
+		T	*_val;
+		int	_nb;
+	public:
+		Array():_val(NULL), _nb(0);
+		Array(T val, int size) : _val(new[size]), _nb(size);
+		~Array()
+		{
+			delete[] _val;
+		};
 
+		Array(const Array &other) : _val(new[other._nb]), _nb(other._nb)
+		{
+			for (int i = 0; i < other._nb; i++)
+				_val[i] = other.[i];
+		};
 
+		Array operator=(const Array &other)
+		{
+			if (*this != other)
+			{
+				delete[] this->_val;
+				this->_nb = other._nb;
+				this->_val = new T[other._nb];
+				for (int i = 0; i < _nb; i++)
+					this->_val[i] = other._val[i];
+				return (*this);
+			}
+			return (*this);
+		};
+
+		T &operator[](int index)
+		{
+			if (index >= size())
+				throw std::out_of_range("Invalid index");
+			return (this->_val[index]);
+		}
+		
+		const T &operator[](int index)
+		{
+			if (index >= size())
+				throw std::out_of_range("Invalid index");
+			return (this->_val[index]);
+		}
+		
+		int size(){return (this->_nb)};
 };
--- cpp-07/ex02/src/main.cpp
+++ cpp-07/ex02/src/main.cpp
@@ -6,15 +6,35 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 09:44:00 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/25 09:44:49 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/25 11:44:47 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include <Array.hpp>
 
 
 int main()
 {
+	Array<int> test1;
+	std::cout << "Def construct--> Array<int> test1" << std::endl;
+	std::cout << "-----------------------------------" << std::endl;
+	std::cout << "Size of test1: " << test1.size() << std::endl;
 
+	std::cout << "====================================\n";
+	Array<int> bob(8);
+
+
+
+
+	try
+	{
+		
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	
 	return (0);
 }
