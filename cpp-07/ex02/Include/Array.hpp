f05be8ddf59fd658ed8016e2780656e9fc5d05f5
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-11-25 12:29:51 +0100

    ok

--- cpp-07/ex02/Include/Array.hpp
+++ cpp-07/ex02/Include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 09:43:25 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/25 11:47:19 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/25 12:29:33 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,17 @@ class Array
 		T	*_val;
 		int	_nb;
 	public:
-		Array():_val(NULL), _nb(0);
-		Array(T val, int size) : _val(new[size]), _nb(size);
+		Array():_val(NULL), _nb(0){};
+		Array(int size) : _val(new T[size]), _nb(size){};
 		~Array()
 		{
 			delete[] _val;
 		};
 
-		Array(const Array &other) : _val(new[other._nb]), _nb(other._nb)
+		Array(const Array &other) : _val(new T[other._nb]), _nb(other._nb)
 		{
 			for (int i = 0; i < other._nb; i++)
-				_val[i] = other.[i];
+				_val[i] = other._val[i];
 		};
 
 		Array operator=(const Array &other)
@@ -51,17 +51,17 @@ class Array
 
 		T &operator[](int index)
 		{
-			if (index >= size())
+			if (index >= size() || index < 0)
 				throw std::out_of_range("Invalid index");
 			return (this->_val[index]);
 		}
 		
-		const T &operator[](int index)
+		const T &operator[](int index) const
 		{
-			if (index >= size())
+			if (index >= size() || index < 0)
 				throw std::out_of_range("Invalid index");
 			return (this->_val[index]);
 		}
 		
-		int size(){return (this->_nb)};
+		int size(){return (this->_nb);}
 };
--- cpp-07/ex02/src/main.cpp
+++ cpp-07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 09:44:00 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/25 11:44:47 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/25 12:24:29 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,71 @@
 int main()
 {
 	Array<int> test1;
-	std::cout << "Def construct--> Array<int> test1" << std::endl;
-	std::cout << "-----------------------------------" << std::endl;
+	std::cout << "Def construct for test1" << std::endl;
 	std::cout << "Size of test1: " << test1.size() << std::endl;
 
 	std::cout << "====================================\n";
 	Array<int> bob(8);
+	std::cout << "Size of bob: " << bob.size() << std::endl;
 
-
-
+	for (int i = 0; i < bob.size(); i++)
+	{
+		bob[i] = i*10+1;
+	}
+	for (int i = 0; i < bob.size(); i++)
+	{
+		std::cout << "bob on " << i << " " << bob[i] << std::endl;
+	}
+	std::cout << "====================================\n";
+	Array<int> papa(bob);
+	std::cout << "Size of papa(copy of bob): " << papa.size() << std::endl;
+	std::cout << "Vaues inside papa\n";
+	for (int i = 0; i < papa.size(); i++)
+	{
+		std::cout << "papa on " << i << " " << papa[i] << std::endl;
+	}
+	
+	std::cout << "====================================\n";
+	std::cout << "Testing Exceptions: \n";
 
 	try
 	{
+		std::cout << "Testing non number value size for bob\n";
+		std::cout << bob['a'] << std::endl;		
 		
 	}
 	catch (std::exception &e)
 	{
 		std::cout << "Error: " << e.what() << std::endl;
+		try
+		{
+			std::cout << "Testing size for bob\n";
+			std::cout << bob[9] << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << "Error: " << e.what() << std::endl;
+			try
+			{
+				std::cout << "Testing negative size for bob\n";
+				std::cout << bob[-2] << std::endl;
+				
+			}
+			catch (std::exception &e)
+			{
+				std::cout << "Error: " << e.what() << std::endl;
+				try
+				{
+					std::cout << "Testing 0 size for bob\n";
+					std::cout << bob[0] << std::endl;
+				}
+				catch (std::exception &e)
+				{
+					std::cout << "Error: " << e.what() << std::endl;
+				}
+				std::cout << "This works with no issues :)\n";
+			}
+		}
 	}
 	
 	return (0);
