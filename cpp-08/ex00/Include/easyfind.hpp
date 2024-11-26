4570abafd943c61afd248c72f44274f486e3ef07
Guillem Forns Sant <gforns-s@car4s2.42barcelona.com>
2024-11-27 00:47:13 +0100

    starting with basics

--- cpp-08/ex00/Include/easyfind.hpp
+++ cpp-08/ex00/Include/easyfind.hpp
@@ -6,16 +6,38 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 12:56:59 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/25 13:40:50 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/27 00:43:32 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
+#include <algorithm>
 
 template< typename T >
-int easyfind(const T v , int nb)
+int easyfind( T &v, int nb)
 {
-	if (v.empty() || )
-	return (0);
+	return (std::find(v.begin(), v.end(), nb));
 }
+
+
+
+
+/*	THIS DOES NOT US THE <algorithm> lib imposed by the subject :)
+template< typename T >
+bool easyfind(const T v , int nb)
+{
+	if (v.empty())
+		return (false);
+	for (unsigned long i = 0; i <= v.size(); i++)
+	{
+		if (v[i] == nb)
+			return (true);
+	}
+	return (false);
+}
+*/
+
+
+// MUST WATCH!!!!!!!!!!!!!
+//https://elearning.intra.42.fr/notions/piscine-c-d07-templates/subnotions
+//https://elearning.intra.42.fr/notions/piscine-c-d08-stl/subnotions/piscine-c-d08-stl-stl/videos/stl-1e60e486-1b8b-4f10-89bd-b2b3871bd3e9
--- cpp-08/ex00/src/main.cpp
+++ cpp-08/ex00/src/main.cpp
@@ -6,24 +6,37 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 12:56:49 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/11/25 13:41:44 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/11/27 00:25:03 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stdexcept>
-#include "easyfind.hpp"
 #include <vector>
+#include <algorithm>
+#include "easyfind.hpp"
+
+
+
+
 
 int main()
 {
-
+	std::vector <int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i + 1);
+	}
+	
+	std::cout << ::easyfind(v, 3);
 
 	return (0);
 }
 
+/*
 https://en.cppreference.com/w/cpp/container/vector
 https://en.cppreference.com/w/cpp/container/map
 https://en.cppreference.com/w/cpp/container/list
 https://en.cppreference.com/w/cpp/container/deque
 https://en.cppreference.com/w/cpp/container/stack
+*/
