cb4cc8e92ce61d6fe03e68b4463e2225b3970b6d
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-12-09 10:19:45 +0100

    extended tests

--- cpp-08/ex00/src/main.cpp
+++ cpp-08/ex00/src/main.cpp
@@ -6,13 +6,14 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 12:56:49 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/12/09 09:14:01 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/12/09 10:19:13 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+#include <list>
 #include <algorithm>
 #include "easyfind.hpp"
 
@@ -26,6 +27,7 @@ int main()
 	}
 	try
 	{
+		std::cout << "Test for vector:" << std::endl;
 		std::vector<int>::iterator v1 = ::easyfind(v, 3);
 		std::cout << *v1 << std::endl;
 	}
@@ -34,6 +36,33 @@ int main()
 		std::cout << e.what() << std::endl;
 	}
 
+	std::list <int>lst;
+	int array[10] = {1, 4, 5, 88, 21, 3, 41, 90, 12, 77};
+	for (int i = 0; i < 10; i++)
+	{
+		lst.push_back(array[i]);
+	}
+	try
+	{
+		std::cout << "Test for list 1: (contains element)" << std::endl;
+		std::list <int>::iterator lst1 = ::easyfind(lst, 41);
+		std::cout << *lst1 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		std::cout << "Test for list 2: (does not contain element)" << std::endl;
+		std::list <int>::iterator lst2 = ::easyfind(lst, 42);
+		std::cout << *lst2 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
 	return (0);
 }
 
