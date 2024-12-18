c49a09ed4c3471c2fe70d533b14a8d85320a2ae5
Guillem Forns Sant <gforns-s@cbr9s1.42barcelona.com>
2024-12-18 18:51:39 +0100

    preparing new

--- cpp-09/ex01/src/main.cpp
+++ cpp-09/ex01/src/main.cpp
@@ -6,8 +6,19 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 17:05:40 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/12/18 17:34:15 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/12/18 17:38:26 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include <stack>
+
+
+int main(int ac, char **av)
+{
+	std::stack <int>val;
+	if (ac < 2)
+		std::cout << "No input found" << std::endl;
+	
+
+}
