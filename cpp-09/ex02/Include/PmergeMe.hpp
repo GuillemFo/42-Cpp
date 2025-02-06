12b9264266c8ea67e61b58631287f06fa020d91d
Guillem Forns Sant <gforns-s@car9s2.42barcelona.com>
2025-02-06 14:27:34 +0100

    starting last ex

---
+++ cpp-09/ex02/Include/PmergeMe.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 14:10:25 by gforns-s          #+#    #+#             */
+/*   Updated: 2025/02/06 14:19:00 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <list>
+#include <exception>
+
+
+class PmergeMe
+{
+	protected:
+		std::string			sequence;
+		std::vector<int>	sVec;
+		std::list<int>		sLis;
+
+		//timer??
+	public:
+		PmergeMe();
+		~PmergeMe();
+		PmergeMe(const PmergeMe &other);
+		PmergeMe &operator=(const PmergeMe &other);
+
+	
+
+};
---
+++ cpp-09/ex02/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 14:10:36 by gforns-s          #+#    #+#             */
+/*   Updated: 2025/02/06 14:15:07 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+//merge-insert sort algorithm
+//https://en.wikipedia.org/wiki/Merge-insertion_sort
+//https://www.geeksforgeeks.org/sorting-by-combining-insertion-sort-and-merge-sort-algorithms/
+
+
+int main(int ac, char **av)
+{
+	
+
+	return (0);
+}
