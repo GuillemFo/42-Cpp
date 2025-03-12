00ff09300583d8e47be6d0ba8f016bac87ad928f
Guillem Forns Sant <gforns-s@cbr8s3.42barcelona.com>
2025-03-12 10:15:18 +0100

    rethinking

--- cpp-09/ex02/src/PmergeMe.cpp
+++ cpp-09/ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:10:39 by gforns-s          #+#    #+#             */
-/*   Updated: 2025/03/12 09:35:37 by gforns-s         ###   ########.fr       */
+/*   Updated: 2025/03/12 09:57:27 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 //Algorithms
 
+// Split in pairs 
 
 
 
@@ -60,46 +61,3 @@ void PmergeMe::sortV(std::vector<int> &vec)
 	vec = sorted;
 }
 
-
-void PmergeMe::sortD(std::deque<int> &deq)
-{
-	{
-	if (deq.size() <= 1)
-		return;
-
-	std::deque<std::pair<int, int> > pairs;
-	int last = -1;
-
-	for (std::deque<int, int>::iterator it = deq.begin(); it != deq.end();)
-	{
-		int first = *it;
-		++it;
-		if (it != deq.end())
-		{
-			int second = *it;
-			++it;
-			if (first > second)
-				std::swap(first, second);
-			pairs.puch_back(std::make_pair(first, second));
-		}
-		else
-			last = first;
-	}
-
-		/// sorting pairs from begin to end creating A and B and and sorting it;
-	//std::sort
-	
-	std::deque<int, int> sorted;
-	for (size_t i = 0; i < pairs.size(); ++i)
-	{
-		sorted.push_back(pairs[i].second);
-	}
-
-	for (size_t i = 0; i < pairs.size(); ++i)
-	{
-		typename T::Iterator pos = std::lower_bound(sorted.begin(), sorted.end(), last);
-		sorted.insert(pos, last);
-	}
-	deq = sorted;
-	}
-}
